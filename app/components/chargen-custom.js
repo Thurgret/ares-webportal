77f34e6b7d35b73e327632ac816e3b7d08e3a31e
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-21 00:09:59 +0000

    test

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -8,6 +8,14 @@ export default Component.extend({
     let self = this;
     this.set('updateCallback', function() { return self.onUpdate(); } );
   },
+
+  model: function(params) {
+    let api = this.gameApi;
+    return RSVP.hash({
+         attributeoptions:  api.request('attributeOptions', { id: this.get('session.data.authenticated.id') }),
+       })
+       .then((model) => EmberObject.create(model));
+},
   
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
