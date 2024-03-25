a9610212dbba91235997d7c552a179bbf9dc1897
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-25 03:01:51 +0000

    test

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -22,7 +22,9 @@ export default Component.extend({
 
     return {
 
-      attributeoption: this.get('char.custom.attributeoption')
+      attributeoption: this.get('char.custom.attributeoption'),
+      first_weapon_proficiency: this.get(char.custom.firstWeaponProficiency),
+      second_weapon_proficiency: this.get(char.custom.secondWeaponProficiency)
 
     }
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
