f7bbbd58ee93223d13adb9858acc73eb42664848
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-26 23:26:38 +0000

    test

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -11,6 +11,8 @@ export default Component.extend({
   culturalFavouredSkillString: null,
   callingFavouredSkillFirstString: null,
   callingFavouredSkillSecondString: null,
+  distinctiveFeatureFirstString: null,
+  distinctiveFeatureSecondString: null,
   
   didInsertElement: function() {
     this._super(...arguments);
@@ -25,6 +27,8 @@ export default Component.extend({
     this.char.custom.cultural_favoured_skill_selection = this.culturalFavouredSkillString;
     this.char.custom.calling_favoured_skill_first_selection = this.callingFavouredSkillFirstString;
     this.char.custom.calling_favoured_skill_second_selection = this.callingFavouredSkillSecondString;
+    this.char.custom.distinctive_feature_first_selection = this.distinctiveFeatureFirstString;
+    this.char.custom.distinctive_feature_second_selection = this.distinctiveFeatureSecondString;
 
     return {
 
@@ -33,7 +37,10 @@ export default Component.extend({
       second_weapon_proficiency: this.get('char.custom.secondWeaponProficiency'),
       cultural_favoured_skill_selection: this.get('char.custom.cultural_favoured_skill_selection'),
       calling_favoured_skill_first_selection: this.get('char.custom.calling_favoured_skill_first_selection'),
-      calling_favoured_skill_second_selection: this.get('char.custom.calling_favoured_skill_second_selection')
+      calling_favoured_skill_second_selection: this.get('char.custom.calling_favoured_skill_second_selection'),
+      distinctive_feature_first_selection: this.get('char.custom.distinctive_feature_first_selection'),
+      distinctive_feature_second_selection: this.get('char.custom.distinctive_feature_second_selection')
+
 
     }
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
