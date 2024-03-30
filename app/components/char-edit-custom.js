eed15ebf9c5d109bcdd085d0c3a13355e5355eab
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-30 00:26:49 +0000

    test

--- app/components/char-edit-custom.js
+++ app/components/char-edit-custom.js
@@ -2,6 +2,10 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: '',
+  wargearDiscardString: null,
+  armourOptionString: null,
+  weaponOptionString: null,
+  shieldOptionString: null,
   
   didInsertElement: function() {
     this._super(...arguments);
@@ -10,9 +14,21 @@ export default Component.extend({
   },
   
   onUpdate: function() {
+
+
+
+    this.char.custom.armour_selection = this.armourOptionString;
+    this.char.custom.weapon_selection = this.weaponOptionString;
+    this.char.custom.shield_selection = this.shieldOptionString;
+    this.char.custom.wargear_discard_selection = this.wargearDiscardString;
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
     // 
     // return { goals: this.get('char.custom.goals') };
-    return {};
+    return {
+      armour_selection: this.get('char.custom.armour_selection'),
+      weapon_selection: this.get('char.custom.weapon_selection'),
+      shield_selection: this.get('char.custom.shield_selection'),
+      wargear_discard_selection: this.get('char.custom.wargear_discard_selection')
+    };
   }
 });
