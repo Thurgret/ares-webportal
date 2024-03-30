a9d4d0330a89c9966141b78197f82fa774dcf1d5
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-30 20:04:41 +0000

    test

--- app/components/char-edit-custom.js
+++ app/components/char-edit-custom.js
@@ -6,6 +6,10 @@ export default Component.extend({
   currentArmourOptionString: null,
   weaponOptionString: null,
   shieldOptionString: null,
+  wargearWearString: null,
+  wargearWieldString: null,
+  wargearDropString: null,
+  wargearStoreString: null,
   
   didInsertElement: function() {
     this._super(...arguments);
@@ -21,6 +25,11 @@ export default Component.extend({
     this.char.custom.weapon_selection = this.weaponOptionString;
     this.char.custom.shield_selection = this.shieldOptionString;
     this.char.custom.wargear_discard_selection = this.wargearDiscardString;
+
+    this.char.custom.wargear_wear_selection = this.wargearWearString;
+    this.char.custom.wargear_wield_selection = this.wargearWieldString;
+    this.char.custom.wargear_drop_selection = this.wargearDropString;
+    this.char.custom.wargear_store_selection = this.wargearStoreString;
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
     // 
     // return { goals: this.get('char.custom.goals') };
@@ -28,7 +37,11 @@ export default Component.extend({
       current_armour_selection: this.get('char.custom.current_armour_selection'),
       weapon_selection: this.get('char.custom.weapon_selection'),
       shield_selection: this.get('char.custom.shield_selection'),
-      wargear_discard_selection: this.get('char.custom.wargear_discard_selection')
+      wargear_discard_selection: this.get('char.custom.wargear_discard_selection'),
+      wargear_wear_selection: this.get('char.custom.wargear_wear_selection'),
+      wargear_wield_selection: this.get('char.custom.wargear_wield_selection'),
+      wargear_drop_selection: this.get('char.custom.wargear_drop_selection'),
+      wargear_store_selection: this.get('char.custom.wargear_store_selection')
     };
   }
 });
